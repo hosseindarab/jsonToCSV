4cc0ea8ed75fa003e86ebfbfbd624a076b749c7f
hosseindarab <hdarab400@gmail.com>
2022-10-04 00:17:59 +0200

    - for loop for more records

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const { parse } = require('json2csv');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 const sendGetRequest = async () => {
     try {
@@ -13,29 +14,32 @@ const sendGetRequest = async () => {
         const resp = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json', config);
         console.log(resp.data);
         var json = [resp.data]
-        const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
         const csvWriter = createCsvWriter({
             path: './csv/file.csv',
             header: [
                 'rowCounter',
                 'time',
-                 'disclaimer',
-                 'chartName',
-                 'bpi'
+                'disclaimer',
+                'chartName',
+                'bpi'
             ],
             fieldDelimiter: ';',
         });
         const records1 = [
             { rowCounter: '1', time: new Date() }
-        ];
+        ]
+        
         const records2 = [
-            { rowCounter:'2',time: json[0].time.updated, disclaimer: json[0].disclaimer, chartName:json[0].chartName, chartName:json[0].bpi.USD.rate_float }
+            { rowCounter: '2', time: json[0].time.updated, disclaimer: json[0].disclaimer, chartName: json[0].chartName, chartName: json[0].bpi.USD.rate_float }
         ];
 
         await csvWriter.writeRecords(records1)
-        await csvWriter.writeRecords(records2)    
-            // returns a promise
-            
+        for (let i = 0; i < 10; i++) {
+            await csvWriter.writeRecords(records2)
+        }
+        // returns a promise
+
         // var fields = Object.keys(json)
         // const opts = { fields };
         // const csv = parse(json, opts);
