4c63c56a5a43996e42c552b600e99098c39c317f
hosseindarab <hdarab400@gmail.com>
2022-10-03 19:50:29 +0200

    - new library used (csv-writer)

--- index.js
+++ index.js
@@ -12,11 +12,34 @@ const sendGetRequest = async () => {
         // };
         const resp = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json', config);
         console.log(resp.data);
-        var json = resp.data
-        var fields = Object.keys(json)
-        const opts = { fields };
-        const csv = parse(json, opts);
-        console.log(csv);
+        var json = [resp.data]
+        const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+        const csvWriter = createCsvWriter({
+            path: './file.csv',
+            header: [
+                'rowCounter',
+                'time',
+                 'disclaimer',
+                 'chartName',
+                 'bpi'
+            ],
+            fieldDelimiter: ';',
+        });
+        const records1 = [
+            { rowCounter: '1', time: new Date() }
+        ];
+        const records2 = [
+            { rowCounter:'2',time: json[0].time.updated, disclaimer: json[0].disclaimer, chartName:json[0].chartName, chartName:json[0].bpi.USD.rate_float }
+        ];
+
+        await csvWriter.writeRecords(records1)
+        await csvWriter.writeRecords(records2)    
+            // returns a promise
+            
+        // var fields = Object.keys(json)
+        // const opts = { fields };
+        // const csv = parse(json, opts);
+        // console.log(csv);
         // var replacer = function (key, value) { return value === null ? '' : value }
         // var csv = json.map(function (row) {
         //     return fields.map(function (fieldName) {
